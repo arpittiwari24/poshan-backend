d40b47dc63183c05fb532bfd34c285690054a08d
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-12-12 21:10:20 +0530

    commit

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import router from "./routes/Allroutes.js"
 import paymentRouter from "./routes/payment.js"
 import blogRouter from "./routes/Blogs.js"
 import adminAuth from "./routes/AdminAuth.js"
-import multer from "multer"
-import path from "path"
 
 //configuration
 
@@ -34,34 +32,6 @@ app.use("/users",router)
 app.use("/payment",paymentRouter)
 app.use("/blogs",blogRouter)
 app.use("/admin",adminAuth)
-
-
- 
-const storage = multer.diskStorage({
-  destination:
- 
-"uploads/",
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
-app.post("/upload", upload.single("image"), async (req, res) => {
-  const imageUrl = `http://localhost:5000/upload/${req.file.filename}`;
-
-  try {
-    // This is just an example, you can replace this with your own logic for converting the image
-    // await convertImage(req.file.path);
-
-    res.json({ imageUrl });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error.message);
-  }
-});
-  
 
 
 // spin up the server
